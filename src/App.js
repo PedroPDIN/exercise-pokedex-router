288b1f66a752b62d0532f50627378c24e6c52a5f
Pedro <pedro.oficial1969@gmail.com>
2021-11-11 13:29:23 -0300

    adiciona exercico 1 concluido

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+    </BrowserRouter>
   );
 }
 
