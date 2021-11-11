0608e452dd5cb6610bd86965eac9d7c3904b08b3
Pedro <pedro.oficial1969@gmail.com>
2021-11-11 13:50:00 -0300

    adiciona exercicio 2 a 5 concluido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import pokemons from './data';
-import Pokedex from './Pokedex';
+/* import pokemons from './data';
+import Pokedex from './Pokedex'; */
+import Home from './components/Home';
+import About from './components/About'
+import Users from './components/Users';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons} />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/users' component={Users} />
       </div>
     </BrowserRouter>
   );
